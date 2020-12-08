9d2193ee2c87430c337751f46f24fb6768e0377e
Ryan Garza <ryangarza@live.com>
2020-12-08 11:19:17 -0600

    buttons css update

--- src/components/ChategoryTopicsList/ChategoryTopicsList.js
+++ src/components/ChategoryTopicsList/ChategoryTopicsList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Container } from '@material-ui/core';
+import { Button, Container } from '@material-ui/core';
 import firebase from "firebase/app"
 import 'firebase/firestore';
 import 'firebase/auth';
@@ -47,7 +47,8 @@ function Topics(props) {
   const { title, id } = props.message;
 return (<>
     <div>
-    <Link to={'/chat/' + id}>{title}</Link>
+    {/* <Link to={'/chat/' + id}>{title}</Link> */}
+    <Button variant="outlined" color="secondary" href={'/chat/' + id}>{title}</Button>    
    </div>
   </>)
 }
