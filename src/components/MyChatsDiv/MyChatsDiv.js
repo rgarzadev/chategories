f28898b767ee59ccc407ef3698c78b74cc137b44
Tea Flores-Kestner <dfkestner@gmail.com>
2020-12-08 20:58:50 -0500

    final touches for DM functionality

--- src/components/MyChatsDiv/MyChatsDiv.js
+++ src/components/MyChatsDiv/MyChatsDiv.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Container, Button } from '@material-ui/core';
 import firebase from '../../firebase';
-import { useCollectionData } from 'react-firebase-hooks/firestore'
-
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 import "./MyChatsDiv.css";
 
 const auth = firebase.auth();
@@ -26,6 +25,7 @@ function MyChatsDiv() {
   };
 
   const { uid } = auth.currentUser;
+
   const dmsRef = firestore.collection('dms');
   const query = dmsRef.where("users", 'array-contains', uid);
   const [dmUsers] = useCollectionData(query, { idField: 'id' });
@@ -35,7 +35,7 @@ function MyChatsDiv() {
     <div className="ContentArea">
       <Container className="MyChatsDiv" maxWidth="sm">
         <div className="container">
-          {dmUsers && dmUsers.map(dmUser => <DM key={dmUser.id} message={dmUser} />)}
+          {dmUsers && dmUsers.map(dmUser => <DM key={dmUser.id} id={dmUser.id} displayName1={dmUser.displayName1} displayName2={dmUser.displayName2}/>)}
         </div>
       </Container>
     </div>
@@ -43,13 +43,21 @@ function MyChatsDiv() {
 }
 
 function DM(props) {
-  const {id} = props.message;
-  console.log(props)
+  const uid = auth.currentUser.uid;
+  const usersRef = firestore.collection('users');
+  const query = usersRef.where('uid', '==', uid);
+  const users = useCollectionData(query, { idField: 'id'});
+  let userName;
+  if (users.displayName !== props.displayName1) {
+    userName = props.displayName2
+  } else {
+    userName = props.displayName1
+  }
+  
   return (
     <div>
       {/* <Link to={'/chat/' + id}>{title}</Link> */}
-      <Button variant="outlined" color="secondary" href={'/chat/' + id}>Direct Message</Button>
-
+      <Button variant="outlined" color="secondary" href={'/chat/' + props.id}>{userName}</Button>
     </div>
   )
 }
